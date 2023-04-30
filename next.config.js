35ab982833c937e7db202a0e0b16bc116b8746c4
RYOJI KITA <baleonly@gmail.com>
2023-04-30 16:58:01 +0900

    tailwind daisyuiの設定変更

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    swcMinify: true,
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
